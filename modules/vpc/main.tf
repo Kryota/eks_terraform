334423a41ba1a5dc491829dfcf3713a121990df4
Ryota <k16koyama@gmail.com>
2020-05-07 20:23:10 +0900

    subnetを作成した

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,8 +3,38 @@ resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name                                = var.name
+    Name                                = "vpc-${var.name}"
     Terraform                           = "true"
     "kubernetes.io/cluster/${var.name}" = "shared"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.subnet_azs, count.index)
+
+  tags = {
+    Name                                = "subnet-${var.name}-public-${count.index}"
+    Terraform                           = "true"
+    "kubernetes.io/cluster/${var.name}" = "shared"
+    "kubernetes.io/role/elb"            = "1"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.subnet_azs, count.index)
+
+  tags = {
+    Name                                = "subnet-${var.name}-private-${count.index}"
+    Terraform                           = "true"
+    "kubernetes.io/cluster/${var.name}" = "shared"
+    "kubernetes.io/role/internal-elb"   = "1"
   }
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,9 +1,24 @@
 variable "vpc_cidr" {
   description = "作成するVPCのCIDRブロック"
-  type = string
+  type        = string
 }
 
 variable "name" {
   description = "タグに使う名前"
-  type = string
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "public subnetのcidr block"
+  type        = list
+}
+
+variable "private_subnet_cidrs" {
+  description = "private subnetのcidr block"
+  type        = list
+}
+
+variable "subnet_azs" {
+  description = "subnetを作成するアベイラビリティゾーン"
+  type        = list
 }
