559de8a4c7ebbe19f6156416b8eb7795e39cb4ac
Ryota <k16koyama@gmail.com>
2020-05-09 21:33:24 +0900

    無駄な空行を消した

--- main.tf
+++ main.tf
@@ -13,9 +13,8 @@ terraform {
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_cidr = "10.100.0.0/24"
-  name     = "kr-eks-handson"
-
+  vpc_cidr             = "10.100.0.0/24"
+  name                 = "kr-eks-handson"
   public_subnet_cidrs  = ["10.100.0.1/28", "10.100.0.16/28"]
   private_subnet_cidrs = ["10.100.0.32/28", "10.100.0.48/28"]
   subnet_azs           = ["ap-northeast-1a", "ap-northeast-1c"]
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -51,8 +51,7 @@ resource "aws_route" "public_internet_gateway" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(var.subnet_azs)
-
+  count          = length(var.subnet_azs)
   subnet_id      = element(aws_subnet.public.*.id, count.index)
   route_table_id = aws_route_table.public.id
 }
