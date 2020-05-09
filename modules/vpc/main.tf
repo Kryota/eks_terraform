47a1a4ace8edac21595b4d3ed2ed65450b9c79aa
Ryota <k16koyama@gmail.com>
2020-05-09 21:30:50 +0900

    Nat Gatewayを作成し，private subnetに紐づけた

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "this" {
 
 # Public Subnet
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnet_cidrs)
+  count             = length(var.subnet_azs)
   vpc_id            = aws_vpc.this.id
   cidr_block        = element(var.public_subnet_cidrs, count.index)
   availability_zone = element(var.subnet_azs, count.index)
@@ -59,7 +59,7 @@ resource "aws_route_table_association" "public" {
 
 # Private Subnet
 resource "aws_subnet" "private" {
-  count             = length(var.private_subnet_cidrs)
+  count             = length(var.subnet_azs)
   vpc_id            = aws_vpc.this.id
   cidr_block        = element(var.private_subnet_cidrs, count.index)
   availability_zone = element(var.subnet_azs, count.index)
@@ -70,4 +70,50 @@ resource "aws_subnet" "private" {
     "kubernetes.io/cluster/${var.name}" = "shared"
     "kubernetes.io/role/internal-elb"   = "1"
   }
+}
+
+# Elastic IP
+resource "aws_eip" "this" {
+  count = length(var.subnet_azs)
+  vpc   = true
+
+  tags = {
+    Name      = "eip-${var.name}-${count.index}"
+    Terraform = "true"
+  }
+}
+
+# Nat Gateway
+resource "aws_nat_gateway" "this" {
+  count         = length(var.subnet_azs)
+  allocation_id = element(aws_eip.this.*.id, count.index)
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+
+  tags = {
+    Name      = "nat-${var.name}-${count.index}"
+    Terraform = "true"
+  }
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.subnet_azs)
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name      = "rt-${var.name}-private-${count.index}"
+    Terraform = "true"
+  }
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count                  = length(var.subnet_azs)
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.this.*.id, count.index)
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.subnet_azs)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
 }
