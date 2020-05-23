f8fa878a98031edf5288431bc06e0efcac0a163e
Ryota <k16koyama@gmail.com>
2020-05-24 02:07:18 +0900

    ouptputしたコマンドにミスがあったので修正した

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "eks_create_command" {
   description = "EKSのクラスター作成時に使用するコマンド"
-  value       = "eksctl create cluster --name eks_handson --vpc-public-subnet ${join(",", module.vpc.public_subnet_ids)} --vpc-private-subnet ${join(",", module.vpc.private_subnet_ids)} --without-nodegroup"
+  value       = "eksctl create cluster --name eks_handson --vpc-public-subnets=${join(",", module.vpc.public_subnet_ids)} --vpc-private-subnets=${join(",", module.vpc.private_subnet_ids)} --without-nodegroup"
 }
