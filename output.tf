682998a86552b6d0188656f97bd37b3f9063ade1
Ryota <k16koyama@gmail.com>
2020-05-24 02:09:30 +0900

    cluster nameにはアンスコは使えなかったので，ハイフンにした

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "eks_create_command" {
   description = "EKSのクラスター作成時に使用するコマンド"
-  value       = "eksctl create cluster --name eks_handson --vpc-public-subnets=${join(",", module.vpc.public_subnet_ids)} --vpc-private-subnets=${join(",", module.vpc.private_subnet_ids)} --without-nodegroup"
+  value       = "eksctl create cluster --name eks-handson --vpc-public-subnets=${join(",", module.vpc.public_subnet_ids)} --vpc-private-subnets=${join(",", module.vpc.private_subnet_ids)} --without-nodegroup"
 }
