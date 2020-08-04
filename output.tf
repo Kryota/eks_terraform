55bdc0adf14c4e10c21ac4c23b7586451b18c44d
Ryota <k16koyama@gmail.com>
2020-08-05 08:54:09 +0900

    alb-ingress-controllerにvpcのidを設定しなければいけないのでoutputするようにしておいた

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "eks_create_command" {
   description = "EKSのクラスター作成時に使用するコマンド"
   value       = "eksctl create cluster --name eks-handson --vpc-public-subnets ${join(",", module.vpc.public_subnet_ids)} --vpc-private-subnets ${join(",", module.vpc.private_subnet_ids)} --without-nodegroup"
 }
+
+output "vpc_id" {
+  description = "VPCのID"
+  value       = module.vpc.vpc_id
+}
